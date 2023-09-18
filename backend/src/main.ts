84d9a7201ea84960c56204fcfc0f2295cbd8769c
Pranav Grandhi <pranavgrandhi@Pranavs-MacBook-Pro.local>
2023-09-17 21:32:44 -0500

    backend: typescript rewrite

---
+++ backend/src/main.ts
@@ -0,0 +1,51 @@
+import { type AddressInfo } from "net";
+
+import express from "express";
+import { GracefulShutdownManager } from "@moebius/http-graceful-shutdown";
+
+const app = express();
+app.disable("x-powered-by");
+
+// ----------------------------------- Middleware -----------------------------------
+// JSON body parsing
+app.use(express.json());
+// request logger
+if (process.env.NODE_ENV === "development") {
+  const morgan = (await import("morgan")).default;
+  app.use(morgan("tiny"));
+}
+
+// ------------------------------------- Routes -------------------------------------
+// health check
+app.get("/healthz", (_, res) => {
+  return res.status(200).json({ status: "available" });
+});
+
+// not found
+app.use((_, res) => {
+  return res.status(404).json({
+    error: {
+      message: "This API endpoint does not exist.",
+      type: "invalid_request_error",
+    },
+  });
+});
+
+// ------------------------------------- Server -------------------------------------
+const server = app.listen(process.env.PORT ?? 8080, () => {
+  console.log(
+    `Listening: http://localhost:${(<AddressInfo>server.address()).port}/`
+  );
+  console.log(`▶️ Environment: ${process.env.NODE_ENV}`);
+});
+
+const shutdownManager = new GracefulShutdownManager(server);
+
+// listen for syscall signals to shut down
+process.on("SIGINT", async () => {
+  console.log("SIGINT signal received: gracefully shutting down...");
+  // stop the server and all connected clients
+  shutdownManager.terminate(() => {
+    console.log("HTTP server closed");
+  });
+});
